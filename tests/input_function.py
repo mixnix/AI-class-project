9cd7e7c35676b620dad024fa441a251b0e22889f
Michał <bukaniex@gmail.com>
2018-05-09 15:15:07 +0900

    modified test files so I don't need configuration files in pycharm to run them

--- tests/input_function.py
+++ tests/input_function.py
@@ -1,7 +1,8 @@
 import pyautogui
 import time
 
-time.sleep(1)
-x = 1250
-y = 20
-pyautogui.click(x,y)
+if __name__ == '__main__':
+    time.sleep(1)
+    x = 1250
+    y = 20
+    pyautogui.click(x,y)
--- tests/take_printscreen.py
+++ tests/take_printscreen.py
@@ -1,9 +1,9 @@
 import pyautogui
 import time
 
-time.sleep(2)
-#Take screenshot
-pic = pyautogui.screenshot()
-
-#Save the image
-pic.save('Screenshot.jpg')
+if __name__ == '__main__':
+    time.sleep(2)
+    #Take screenshot
+    pic = pyautogui.screenshot()
+    #Save the image
+    pic.save('Screenshot.jpg')
