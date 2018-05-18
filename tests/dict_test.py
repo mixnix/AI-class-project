8c0593e1fc11e303732182b21728182d63488ce8
Michał <bukaniex@gmail.com>
2018-05-18 21:37:28 +0900

    tested how to iterate over dictionary

---
+++ tests/dict_test.py
@@ -0,0 +1,13 @@
+
+
+def iterating_over_dict_test():
+    dictionary = {
+        "key1" : 1,
+        "key2" : 2,
+        "key3" : 3,
+    }
+    for key in dictionary:
+        print(key)
+
+if __name__ == "__main__":
+    iterating_over_dict_test()
